4713674cbcdac37e62030b96b937bbf5d0a26513
M-alpha-bet <fridayokeomamartin@gmail.com>
2025-09-02 14:47:56 +0100

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,86 @@
+"use client";
 import Image from "next/image";
+import { FaGoogle } from "react-icons/fa";
+import { TbArrowForwardUpDouble } from "react-icons/tb";
+import { motion } from "motion/react";
+import { signIn } from "next-auth/react";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="text-center px-4">
+          <motion.h1
+            className="font-semibold header-text"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            AI Knowledge <br />
+            <span className="purple-text-gradient">Assistant</span>
+          </motion.h1>
+          <motion.p
+            className="pb-10 mt-4"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.6, duration: 0.6 }}
+          >
+            Upload your document, ask questions and get answers
+          </motion.p>
+          <div className="relative flex translate-x-[50px] md:translate-x-0 md:justify-center items-start mt-3 md:my-7">
+            <motion.div
+              className="relative"
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{ delay: 1, type: "spring", stiffness: 150 }}
+            >
+              <motion.div
+                initial={{ y: 0, rotate: 0 }}
+                animate={{
+                  y: [0, -8, 0],
+                  rotate: [0, -3, 3, 0],
+                }}
+                transition={{
+                  duration: 4,
+                  repeat: Infinity,
+                  ease: "easeInOut",
+                }}
+                className="relative"
+              >
+                <Image
+                  src="/images/robo-assistant.png"
+                  alt="Robot assistant"
+                  width={100}
+                  height={100}
+                />
+              </motion.div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+              <div className="absolute -top-6 left-full ml-4 bg-black1 text-white rounded-2xl px-4 py-2 shadow-md w-max max-w-[200px]">
+                I'm here to <br /> help...
+              </div>
+
+              <div className="absolute top-2 left-full ml-1 flex flex-col items-start space-y-1">
+                <span className="w-3 h-3 bg-black1 rounded-full"></span>
+                <span className="w-2 h-2 bg-black1 rounded-full"></span>
+                <span className="w-1 h-1 bg-black1 rounded-full"></span>
+              </div>
+            </motion.div>
+          </div>
+          <motion.div
+            className="mt-20 button-container-gradient hover:scale-[0.95] transform cursor-pointer"
+            onClick={() => signIn("google")}
+            initial={{ scale: 0 }}
+            animate={{ scale: [0, 1.1, 0.95, 1] }}
+            transition={{ delay: 1.8, duration: 0.6, ease: "easeOut" }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="py-2 px-3 rounded-full bg-purple2">
+              <FaGoogle className="inline text-white1" />
+            </div>
+            Continue with google
+            <TbArrowForwardUpDouble className="inline size-5" />
+          </motion.div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
